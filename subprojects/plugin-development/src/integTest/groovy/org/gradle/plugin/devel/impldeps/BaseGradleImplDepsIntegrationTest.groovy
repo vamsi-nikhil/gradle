7d154e5ee7d009b7a901d4d5e3889b1a44ffe315
Benjamin Muskalla <bmuskalla@gradle.com>
2021-02-15 11:38:54 +0100

    fix

--- subprojects/plugin-development/src/integTest/groovy/org/gradle/plugin/devel/impldeps/BaseGradleImplDepsIntegrationTest.groovy
+++ subprojects/plugin-development/src/integTest/groovy/org/gradle/plugin/devel/impldeps/BaseGradleImplDepsIntegrationTest.groovy
@@ -59,7 +59,7 @@ abstract class BaseGradleImplDepsIntegrationTest extends AbstractIntegrationSpec
     static String junitDependency() {
         """
             dependencies {
-                testImplementation 'junit:junit:4.13'
+                testImplementation 'junit:junit:4.13.1'
             }
         """
     }
