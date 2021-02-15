1e1fea80c3beb61ab9fc8462a2f84a06b1db728d
Benjamin Muskalla <bmuskalla@gradle.com>
2021-02-15 15:44:17 +0100

    fixes

--- subprojects/docs/src/snippets/testKit/automaticClasspathInjectionQuickstart/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/testKit/automaticClasspathInjectionQuickstart/kotlin/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     testImplementation("org.spockframework:spock-core:2.0-M4-groovy-3.0") {
         exclude(module = "groovy-all")
     }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
 }
 // end::automatic-classpath[]
 
