7b764b686cdbbd9abffa47e74b9cbf91e2dcb748
Benjamin Muskalla <bmuskalla@gradle.com>
2021-02-16 12:40:02 +0100

    fixes

--- subprojects/docs/src/snippets/testKit/manualClasspathInjection/kotlin/build.gradle.kts
+++ subprojects/docs/src/snippets/testKit/manualClasspathInjection/kotlin/build.gradle.kts
@@ -9,6 +9,7 @@ dependencies {
     testImplementation("org.spockframework:spock-core:2.0-M4-groovy-3.0") {
         exclude(module = "groovy-all")
     }
+    testImplementation("org.junit.jupiter:junit-jupiter-api")
 }
 
 repositories {
